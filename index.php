594b807075bbce761986ab9481710087af452c97
NosirbekDev9373 <nosirbeknurullayev9373@gmail.com>
2024-12-12 19:22:20 +0500

    echo and print full

--- index.php
+++ index.php
@@ -2,15 +2,21 @@
  // php kod yozamiz
 
 
-$variable = "o'zgaruvchi";
+$txt1 = "php tilini urganish";
+$txt2 = " nmadir.com";
+$x = 5;
+$y = 6;
 
-$Ism = 'Nosirbek Nurullayev';
+//echo da ishlash oddiygina
+echo '<h2>' . $txt1 . '</h2>';
+echo '<p>urganish php ' . $txt2 . '</p>';
 
-$ism = 'yangi ism';
+echo $x + $y;
 
-echo "<h2>PHP is Fun!</h2>";
-echo "Salom PHP!<br>";
-echo "PHP da matin yozamiz<br>";
-echo "ushbu ", "qator ", "bilan ", "elementalarni ", "yaratamiz.";
+//prin buyrug'i bilan ham xuddi shunday
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
+
 ?>
  
