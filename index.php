09f6eaed81caa988677d7c54117f9540794979b5
NosirbekDev9373 <nosirbeknurullayev9373@gmail.com>
2024-12-12 18:35:05 +0500

    new1

--- index.php
+++ index.php
@@ -8,13 +8,12 @@ $Ism = 'Nosirbek Nurullayev';
 
 $ism = 'yangi ism';
 
-// case-sensetive - katta kichkina harflar ahamiyatli  
-
-echo $variable . ' ' . $ism ;
-
-
-
-
-
+function qoshish(){
+	global $ism;
+	$a = '';
+	$b = '';
+	echo $ism;
+}
+qoshish();
 ?>
  
