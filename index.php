aba21a6d8b78191e5bcd7f9d1e48971e97357fc7
NosirbekDev9373 <nosirbeknurullayev9373@gmail.com>
2024-12-12 16:54:40 +0500

    php1

---
+++ index.php
@@ -0,0 +1,20 @@
+ <?php  
+ // php kod yozamiz
+
+
+$variable = "o'zgaruvchi";
+
+$Ism = 'Nosirbek Nurullayev';
+
+$ism = 'yangi ism';
+
+// case-sensetive - katta kichkina harflar ahamiyatli  
+
+echo $variable . ' ' . $ism ;
+
+
+
+
+
+?>
+ 
