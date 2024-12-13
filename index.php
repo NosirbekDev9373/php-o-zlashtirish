76f89affec461b4f6bf99ce34aafaf75d0c5df66
NosirbekDev9373 <nosirbeknurullayev9373@gmail.com>
2024-12-13 19:57:03 +0500

    strings

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
  <?php  
  // php kod yozamiz
+ $gap = 'salom 123 ismli Komil';
 
-/*
-String
-Integer
-Float (floating point numbers - also called double)
-Boolean
-Array
-Object
-NULL
-Resource
-*/
-$string = 'Salom dunyo';
-$integer = '123612764871';
-$float = '43.5';
-$boolean = 'true';//	true = 1 , false = 0
-$array = ['birinchi', 'ikkinchi', 3, 43.6, true];
-$array2 = array('birinchi', 'ikkinchi', 3, 43.6, true);
+ echo trim($gap);
+
 
 ?>
  
