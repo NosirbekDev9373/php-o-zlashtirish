3a1854296e32a89a769cbb198b88a07f5924aef5
NosirbekDev9373 <nosirbeknurullayev9373@gmail.com>
2024-12-16 13:03:36 +0500

    php loops while, for, foreach

--- index.php
+++ index.php
@@ -1,37 +1,60 @@
  <?php  
 
-// Switch
+// Loops
 
- $rang = 'yashil';
+/*
+While
+do..while
+for
+foreach
+*/
+/*
+for ( init counter; test counter; increment counter){
+	code to be executed for each iteration;
+}
+*/
 
- switch ($rang) {
- 	case 'qizil':
- 	case 'sariq':
- 	    echo 'rang sariqga teng';
- 	    break;
-    case 'kuk':
-    case 'oq':
-         echo 'kuk rang chiqar';
-         break;
- 	case 'yashil':
- 	    echo 'yashil ga teng busa chiq';
- 	    break;
- 	default:     
- 	    echo 'bu rang bizda yo\'q';	
- }
+/*
+for($x = 1; $x <= 10; $x++){ //bu Loops turiga kiradi
+	echo $x . '<br>';
+}
+*/
 
- if($rang == 'yashil'){
- 	echo ' rang yashilga teng';
- } elseif ($rang == 'qizil') {
- 	echo 'rang qizilga teng';
- } else {
- 	echo 'bu rang bizda yoq';
- }
+// while ga misol
+
+/*
+$x = 1;
+
+while ($x <= 5) {
+	echo $x . '<br>';
+	$x++;
+}
+
+*/
+
+// foreach ga misol
+
+$royhat = ['yashil', 'qizil', 'sariq'];
+$yosh = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"40");
+/* foreach ($arry as #value) {
+	code to be executed;
+}
+*/
+
+foreach($yosh as $odam => $yoshi){
+   echo $odam . ' - ' . $yoshi . '<br>';
+}
 
 
 
+// bunda bizga 4 qoldirirb ket deb ayty
 
-
-
-
+/*
+for($x = 1; $x <= 5; $x++) {
+    if($x == 4){
+    	continue;
+    }
+    echo $x . '<br>';
+}
+*/
  ?>
